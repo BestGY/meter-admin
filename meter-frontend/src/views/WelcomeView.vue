efea576a441448bc8621873f699a30eeb2f26584
tomgy <1948581641@qq.com>
2023-09-19 01:50:54 +0800

    深色模式实现

--- meter-frontend/src/views/WelcomeView.vue
+++ meter-frontend/src/views/WelcomeView.vue
@@ -9,7 +9,7 @@
           src="https://up.deskcity.org/pic_source/fe/6a/46/fe6a46bbcafa096d992d980c85fe46a6.jpg"/>
     </div>
     <div class="welcome-title">
-      <div style="font-size: 60px;font-weight: bold;">远传抄表管理平台</div>
+      <div style="font-size: 60px;font-weight: bold;">远传水表管理平台</div>
       <div style="margin-top: 20px">智慧水务</div>
       <div style="margin-top: 10px">远传抄表，在线查询，一站式服务</div>
     </div>
@@ -28,15 +28,15 @@
 <style scoped>
 .welcome-title{
   position: absolute;
-  bottom: 300px;
-  left: 200px;
+  bottom: 380px;
+  left: 380px;
   color: yellow;
   text-shadow: 0 0 10px black;
 }
 .right-card{
   width: 400px;
   z-index: 1;
-  background-color: white;
+  background-color: var(--el-bg-color);
 }
 
 </style>
